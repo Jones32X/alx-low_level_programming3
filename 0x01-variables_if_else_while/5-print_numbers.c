27f416d46858e8d28ca9442ba36bf60c6494068a
Jones32X <jonespedro.Nezingu.com>
2023-12-08 05:04:29 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from base 10
+ * Retur: 0 Success
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf ("\n");
+
+	return (0);
+
+}
