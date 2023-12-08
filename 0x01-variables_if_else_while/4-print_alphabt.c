95f63403acd4221c675c035c0661b06d0f1d5ab7
Jones32X <jonespedro.Nezingu.com>
2023-12-08 04:53:57 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+/**
+ * main - prints alphabet except e and q
+ * Return: 0 successs
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
